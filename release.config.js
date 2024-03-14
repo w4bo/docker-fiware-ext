ccd657ee25b18751d8e5ef2c46ebf349095e557e
w4bo <m.francia@unibo.it>
2024-03-14 16:04:37 +0100

    fix(ci): add release to dockerhub

--- release.config.js
+++ release.config.js
@@ -4,14 +4,14 @@ git push --force origin \${nextRelease.version}
 `;
 const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    ["@semantic-release/exec", {
-        "publishCmd": publishCmd,
-    }],
-    ["@semantic-release/github", {
-        "assets": [
-           // {"path": "welaser/testScripts/*.pdf", "label": "Charts"}
-        ]
-    }],
-    "@semantic-release/git",
+    ["@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }],
+    ["@semantic-release/github",
+        {
+            "assets": [ /* {"path": "welaser/testScripts/*.pdf", "label": "Charts"} */]
+        }],
+    "@semantic-release/git"
 )
 module.exports = config
