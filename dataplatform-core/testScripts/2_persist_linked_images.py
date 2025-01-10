4343fa4c9fabc199d4a7f4fc6edb24ee42190c45
Alex Baiardi <alex.baiardi99@gmail.com>
2025-01-10 16:38:37 +0100

    fix: add push of multiple entity

--- dataplatform-core/testScripts/2_persist_linked_images.py
+++ dataplatform-core/testScripts/2_persist_linked_images.py
@@ -3,6 +3,7 @@ import time
 from dotenv import dotenv_values
 import os
 import subprocess
+import uuid
 
 conf = dotenv_values("../.env")
 orion_url = "http://{}:{}/v2/".format(conf["ORION_IP"], conf["ORION_PORT_EXT"])
@@ -23,22 +24,27 @@ if os.path.exists(image_archive_path):
 ###############################################################################
 
 id = f"urn:ngsi-ld:Camera:{domain}:12cde24e-3e00-4b23-b99a-5e6e2224eeb1"
-imageSnapshot = "https://dummyimage.com/600x400/000/999.jpg"
-data = {
-    "id": id,
-    "type": "Camera",
-    "name": "DroneCam",
-    "domain": domain,
-    "location": {
-        "type": "Point",
-        "coordinates": [10.1234, 20.5678]
-    },
-    "imageSnapshot": imageSnapshot
-}
-headers = {"Content-Type": "application/json"}
-response = requests.post(orion_url + "entities?options=keyValues", json=data, headers=headers)
-assert response.status_code == 201, f"Failed to write data to Orion. Status code {response.status_code}"
+for _ in range(20):
+    imageSnapshot = "https://dummyimage.com/600x400/000/999.jpg"
+    data = {
+        "id": f"urn:ngsi-ld:Camera:{domain}:{uuid.uuid4()}",
+        "type": "Camera",
+        "name": "DroneCam",
+        "domain": domain,
+        "location": {
+            "type": "Point",
+            "coordinates": [10.1234, 20.5678]
+        },
+        "imageSnapshot": imageSnapshot
+    }
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(orion_url + "entities?options=keyValues", json=data, headers=headers)
+    assert response.status_code == 201, f"Failed to write data to Orion. Status code {response.status_code}"
 time.sleep(20)
+
+print([os.path.join(root, file) 
+            for root, _, files in os.walk(image_archive_path) 
+            for file in files])
 
 countFiles = 0
 i = 0
